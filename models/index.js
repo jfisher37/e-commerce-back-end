a30c4098ded6f161e850b9c6a7725603aab01ee8
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-14 15:41:09 -0400

    finished category routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, { through: ProductTag, as: "product-tags", foreignKey: "product_id"});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, { through: ProductTag, as: "tags-products", foreignKey: "tag_id"});
 
 module.exports = {
   Product,
